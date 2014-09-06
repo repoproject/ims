ea08ecf5baf7e1df4db2ed4b652f4108d887e086
repo_user <repo_user@163.com>
2014-09-06 22:12:40 +0800

    修改入库页面和出库页面 by gq

--- source/WebRoot/js/localJS/in.js
+++ source/WebRoot/js/localJS/in.js
@@ -19,11 +19,26 @@ function init()
 function initInput()
 {
 	//alert(22);
+	var d = new Date();
+	//获取当前日期 YYYU-MM-DD
+	var today=  getEditableReportColValues('in_edit','report1',{indate:true});
+	if(today=null)
+	{
+		today=d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
+	}		
 	
-	setEditableReportColValue('in_edit','report1',{indate:"2014-01-01",num:"10"});
+	setEditableReportColValue('in_edit','report1',{indate:today,num:"10"});
 	//setReportInputBoxValue('in_edit','report1',false,{indate:'2013-01-01',num:'10'});
 	//alert(11);
 }
+
+function test1()
+{
+	alert("1111");
+	
+	
+	}
+
 
 function testTypePromptCallBack1(textObj,colvaluesArr)
 {
