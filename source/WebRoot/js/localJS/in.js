3e3de3c5a5f1996fb2984f02f0c2b45f5de260b7
hulu2002 <15901289449@163.com>
2014-09-07 21:33:36 +0800

    入库增加货号名称是否存在的判断 by gq

--- source/WebRoot/js/localJS/in.js
+++ source/WebRoot/js/localJS/in.js
@@ -18,12 +18,26 @@ function init()
  */
 function initInput()
 {
-	//alert(22);
-	indate.displaytype = "";
+		
+	var dataObj = getEditableReportColValues('in_edit','report1',{indate:true,num:true});
 	
-	setEditableReportColValue('in_edit','report1',{indate:"2014-01-01",num:"10"});
-	//setReportInputBoxValue('in_edit','report1',false,{indate:'2013-01-01',num:'10'});
-	//alert(11);
+
+	var myDate = dataObj.indate.value;
+	var num1 = dataObj.num.value;
+	
+	//新增的页面，需要给入库时间默认值
+	if(myDate=="")
+	{
+		var d=new Date();
+		myDate= d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
+	}
+	//新增的页面，需要给入库数量默认值
+	if(num1=="")
+	{
+		num1="0";
+	}
+	//给入库时间和数量默认值
+	setEditableReportColValue('in_edit','report1',{indate:myDate,num:num1});
 }
 
 function testTypePromptCallBack1(textObj,colvaluesArr)
@@ -31,8 +45,8 @@ function testTypePromptCallBack1(textObj,colvaluesArr)
 	 //alert("aaaa");
 	 if(textObj==null) 
 		 return; 
-     var no = colvaluesArr[0].value; 
-     var name = colvaluesArr[1].value; 
+     var name = colvaluesArr[0].value; 
+     var no = colvaluesArr[1].value; 
      setEditableReportColValue('in_edit','report1',{catno:no,catname:name});
 }
 
--- source/src/com/ims/interceptor/InEdit.java
+++ source/src/com/ims/interceptor/InEdit.java
@@ -4,7 +4,12 @@
 package com.ims.interceptor;
 
 import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+
+import com.ims.util.DBUtil;
 import com.wabacus.config.component.application.report.ReportBean;
 import com.wabacus.system.ReportRequest;
 import com.wabacus.system.buttons.EditableReportSQLButtonDataBean;
@@ -13,6 +18,8 @@ import com.wabacus.system.component.application.report.configbean.editablereport
 import com.wabacus.system.component.application.report.configbean.editablereport.EditableReportInsertDataBean;
 import com.wabacus.system.component.application.report.configbean.editablereport.EditableReportUpdateDataBean;
 import com.wabacus.system.intercept.AbsInterceptorDefaultAdapter;
+
+
 
 /**
  * @author ChengNing
@@ -25,22 +32,53 @@ public class InEdit extends AbsInterceptorDefaultAdapter {
 			AbsEditableReportEditDataBean editbean) {
 		if (editbean instanceof EditableReportInsertDataBean) {
 			//editbean.get
+			
+			String strcatno= mRowData.get("catno").trim();
+			
+			String strcatname= mRowData.get("catname").trim();
+			//判断该试剂/耗材是否在库中存在，不存在则提示并不能保存
+			if(!this.addIsExit(strcatno, strcatname))			
+			{
+				 rrequest.getWResponse().getMessageCollector().alert("不存在货号为["+strcatno+"]名称为["+strcatname+"]的试剂/耗材，请提前维护后再进行入库！",null,false);
+					return WX_RETURNVAL_TERMINATE;
+			}			
+			super.doSavePerRow(rrequest, rbean, mRowData, mParamValues,editbean);
 		} else if (editbean instanceof EditableReportUpdateDataBean) {
 
+			super.doSavePerRow(rrequest, rbean, mRowData, mParamValues,editbean);
 		} else if (editbean instanceof EditableReportSQLButtonDataBean) {
 
+			super.doSavePerRow(rrequest, rbean, mRowData, mParamValues,editbean);
 		} else if (editbean instanceof EditableReportDeleteDataBean) {
 			// 删除操作
-
+			super.doSavePerRow(rrequest, rbean, mRowData, mParamValues,editbean);
 		}
 		return WX_RETURNVAL_SUCCESS;
 	}
 	
-	/**
-	 * 入库操作
+
+	/***
+	 * 
+	 * 入库操作时判断是否存在该试剂/耗材
+	 *
+	 * @param strcatno 货号
+	 * @param strcatname 名称
+	 * @return true代表库中有该试剂/耗材，false代表库中不存在该试剂/耗材
 	 */
-	private void add(){
+	private boolean addIsExit(String strcatno,String strcatname ){
 		
+		//判断货号和名称是否存在的sql
+		String sql = "SELECT DISTINCT id FROM d_catcode where catno=?  and  catname=? ";
+		 
+		//执行SQL
+		List<Object> list = DBUtil.query(sql,strcatno, strcatname);
+		//试剂耗材库中不存在该货号
+		if(list.size() <1 ){
+    		return false;
+		}
+		else{
+			return true;
+		}
 	}
 	
 	private void update(){
